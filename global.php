39120d07c0d353482e4e0dc7de621e3d37bc3d47
Wilker Alves <wilkeralves97@gmail.com>
2018-01-10 15:50:34 -0200

    acerto do auto load

--- global.php
+++ global.php
@@ -1,4 +1,10 @@
 <?php
-    require __DIR__ . '/vendor/autoload.php';
     require_once("class/config.php");
+    spl_autoload_register(function($className){
+        if(file_exists("class/".$className.".php")){
+            require_once("class/".$className.".php");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         class/".$className.".php");
+        }
+    }
+    
+    )
 ?>
